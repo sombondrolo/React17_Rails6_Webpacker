d295b5b3706eab1dd2ad8fb8b6cb95128bef2e87
Fer <sombondrolo@gmail.com>
2022-01-02 23:14:58 -0300

    added components folder, created and imported App.jsx

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+class App extends React.Component {
+  render () {
+    return (
+      <h1>Hello App.jsx</h1>
+    )
+  }
+}
+
+export default App
--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -5,6 +5,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import App from "../components/App"
 
 const Hello = props => (
   <div>Hello {props.name}!</div>
@@ -21,7 +22,7 @@ Hello.propTypes = {
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     // <Hello name="React" />,
-    <Hello />,
+    <App />,
     document.body.appendChild(document.createElement('div')),
   )
 })
